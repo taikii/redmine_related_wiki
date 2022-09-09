052c105299378119cc3fe7342f8dde99d8bfa7fb
taikii <taikii.git@outlook.com>
2022-09-10 02:25:48 +0900

    Update init.rb
    Update version

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_related_wiki do
   name 'Redmine Related Wiki plugin'
   author 'taikii'
   description 'This plug-in shows links to Wiki related to Issue in the sidebar.'
-  version '1.0.1'
+  version '1.0.2'
   url 'https://github.com/taikii/redmine_related_wiki'
   author_url 'https://taikii.net/'
 
